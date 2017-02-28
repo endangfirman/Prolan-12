31e4e2c9c0b8a341ef991e7b25751b66f2332527
endangfirman <nakiri66@gmail.com>
2017-02-28 21:09:22 +0700

    Add files via upload

---
+++ Minggu 2/mahasiswa.java
@@ -0,0 +1,7 @@
+public class mahasiswa {
+  public static void main(String argv []){
+      System.out.println("Nama	: Endang Firman");
+	  System.out.println("NIM	: 10112613");
+	  System.out.println("Alamat	: Bandung");
+	  }
+}
